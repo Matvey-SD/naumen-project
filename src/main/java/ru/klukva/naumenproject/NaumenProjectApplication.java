4ddd7bb5e6519bb7bd8d81808f1c49f90f0d88d1
Matthew <sarapulov.03@mail.ru>
2022-10-03 13:22:16 +0500

    Initial commit

---
+++ src/main/java/ru/klukva/naumenproject/NaumenProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.klukva.naumenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaumenProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/klukva/naumenproject/NaumenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.klukva.naumenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaumenProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
